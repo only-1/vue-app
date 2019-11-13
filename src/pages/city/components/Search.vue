2730c38c5e3f114710208dd5db22481c9aebb6d5
郑宏振 <1456421562@qq.com>
2019-11-13 22:42:09 +0800

    search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search">
+    <input type="text"
+    placeholder="输入城市名或拼音"
+    class="city-search">
+  </div>
+</template>
+<script>
+export default {
+  name:'search'
+}
+</script>
+<style lang="stylus" scoped>
+.search
+  height 0.72rem
+  padding:0 .1rem
+  background:red
+  .city-search
+    height 0.62rem
+    line-height .62rem
+    text-align:center
+    width: 100%;
+    border-radius:.06rem
+    color:#666
+
+</style>
